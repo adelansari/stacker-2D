624a9e91489d22416e373609387bb31571637b78
Adel Ansari <adelansari.a@gmailcom>
2024-02-25 01:19:46 +0200

    fix code and styling

--- src/elements.ts
+++
@@ -1,6 +0,0 @@
-export const grid: HTMLElement | null = document.querySelector(".grid");
-export const stackBtn: HTMLElement | null = document.querySelector(".stack");
-export const scoreCounter: HTMLElement | null = document.querySelector(".score-counter");
-export const endGameScreen: HTMLElement | null = document.querySelector(".end-game-screen");
-export const endGameText: HTMLElement | null = document.querySelector(".end-game-text");
-export const playAgainButton: HTMLElement | null = document.querySelector(".play-again");
